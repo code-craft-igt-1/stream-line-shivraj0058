a3c40651cb8330a9612ec0e384c51c34bfe271bc
S <Shivaraja.S@philips.com>
2024-09-24 23:42:27 +0530

    modify limits

--- include/sender.h
+++ include/sender.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "SensorStub.h"
 
-const double TEMPERATURE_UPPER_LIMIT = 102;
-const double TEMPERATURE_LOWER_LIMIT = 96;
+const double TEMPERATURE_UPPER_LIMIT = 115;
+const double TEMPERATURE_LOWER_LIMIT = 52;
 
-const double PULSERATE_UPPER_LIMIT = 100;
-const double PULSERATE_LOWER_LIMIT = 60;
+const double PULSERATE_UPPER_LIMIT = 180;
+const double PULSERATE_LOWER_LIMIT = 20;
 
-const double SPO2_UPPER_LIMIT = 90;
+const double SPO2_UPPER_LIMIT = 50;
 const double SPO2_LOWER_LIMIT = 100;
