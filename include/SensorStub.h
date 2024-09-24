b25318ddb078cf0ed01ffbe704c91570a92fa392
S <Shivaraja.S@philips.com>
2024-09-24 23:00:46 +0530

    fix build

--- include/SensorStub.h
+++ include/SensorStub.h
@@ -1,45 +1,17 @@
+#pragma once
 
-#include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 
 class SensorStub {
-public:
+public: 
     SensorStub() {
         std::srand(static_cast<unsigned int>(std::time(nullptr)));
     }
 
-    double generateRandomSensorValue (double minValue, double maxValue) const {
+    double generateRandomSensorValue(double minValue, double maxValue) const {
         double f = static_cast<double>(std::rand()) / RAND_MAX;
         return minValue + f * (maxValue - minValue);
     }
 };
-
-
-#include <gtest/gtest.h>
-#include "SensorStub.h"
-
-TEST(SensorStubTest, GenerateRandomSensorValue) {
-    SensorStub sensor;
-
-    // Test case 1: minValue = 0, maxValue = 10
-    double minValue1 = 0.0;
-    double maxValue1 = 10.0;
-    double value1 = sensor.generateRandomSensorValue(minValue1, maxValue1);
-    EXPECT_GE(value1, minValue1);
-    EXPECT_LE(value1, maxValue1);
-
-    // Test case 2: minValue = -5, maxValue = 5
-    double minValue2 = -5.0;
-    double maxValue2 = 5.0;
-    double value2 = sensor.generateRandomSensorValue(minValue2, maxValue2);
-    EXPECT_GE(value2, minValue2);
-    EXPECT_LE(value2, maxValue2);
-
-    // Add more test cases as needed
-}
-
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
--- src/sender.cpp
+++ src/sender.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
 #include <cstdlib>
-#include "src/sender.h"
-
-void sendVitalValue(const SensorStub& obj, double vitalminVal, double vitalmaxval) {
-    double val=obj.generateRandomSensorValue(vitalminVal,vitalmaxval);
-    std::cout<<val<<std::endl;
-}
+#include "../include/sender.h"
 
 void sendVitalValues(){
     SensorStub obj;
-    for(int i=0; i<50; i++) {
-        sendVitalValue(obj,TEMPERATURE_LOWER_LIMIT,TEMPERATURE_UPPER_LIMIT);
-        sendVitalValue(obj,PULSERATE_LOWER_LIMIT,PULSERATE_UPPER_LIMIT);
-        sendVitalValue(obj,SPO2_LOWER_LIMIT,SPO2_UPPER_LIMIT);
+    for(int i = 1; i <= 50; i++) {
+        std::cout << obj.generateRandomSensorValue(TEMPERATURE_LOWER_LIMIT,TEMPERATURE_UPPER_LIMIT) << ",";
+        std::cout << obj.generateRandomSensorValue(PULSERATE_LOWER_LIMIT,PULSERATE_LOWER_LIMIT) << ",";
+        std::cout << obj.generateRandomSensorValue(SPO2_LOWER_LIMIT,SPO2_UPPER_LIMIT) << std::endl;
     }
 }
 int main() {
