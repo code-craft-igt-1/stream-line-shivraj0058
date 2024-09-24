cd36a5872c33a0f8ac3ddc2a726a3133c7ab5e8c
S <Shivaraja.S@philips.com>
2024-09-24 23:04:30 +0530

    fix build

--- src/sender.cpp
+++ src/sender.cpp
@@ -2,12 +2,13 @@
 #include <cstdlib>
 #include "../include/sender.h"
 
-void sendVitalValues(){
+void sendVitalValues() {
     SensorStub obj;
-    for(int i = 1; i <= 50; i++) {
-        std::cout << obj.generateRandomSensorValue(TEMPERATURE_LOWER_LIMIT,TEMPERATURE_UPPER_LIMIT) << ",";
-        std::cout << obj.generateRandomSensorValue(PULSERATE_LOWER_LIMIT,PULSERATE_LOWER_LIMIT) << ",";
-        std::cout << obj.generateRandomSensorValue(SPO2_LOWER_LIMIT,SPO2_UPPER_LIMIT) << std::endl;
+    for ( int i = 1; i <= 50; i++ ) {
+        std::cout <<
+        obj.generateRandomSensorValue(TEMPERATURE_LOWER_LIMIT,TEMPERATURE_UPPER_LIMIT) << ", " <<
+        obj.generateRandomSensorValue(PULSERATE_LOWER_LIMIT,PULSERATE_LOWER_LIMIT) << ", " <<
+        obj.generateRandomSensorValue(SPO2_LOWER_LIMIT,SPO2_UPPER_LIMIT) << std::endl;
     }
 }
 int main() {
